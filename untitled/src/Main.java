ef05fa1f6b286135da0a137f4efa4b4c76210246
전산J0403-04\user <cjfwls05324@naver.com>
2024-04-11 18:55:37 +0900

    2024-04-11 JAVA 튜터링 System.out.println("Hello World");

---
+++ untitled/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Hello World 출력
+        System.out.println("Hello World");
+    }
+}
