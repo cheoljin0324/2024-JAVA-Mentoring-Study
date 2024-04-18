8408d2dc3387052a449761a7dbad204b4ef39ca7
cjfwls05324 <77821929+cheoljin0324@users.noreply.github.com>
2024-04-18 18:26:43 +0900

    Revert "2024-04-18 JAVA 튜터링(변수 선언, 초기화, 참조 변수)"
    This reverts commit 659fc42cbbeb3da4f8745c33f8ecc5af9b08c337.

--- untitled1/src/Example_4.java
+++
@@ -1,9 +0,0 @@
-public class Example_4 {
-    public static void main(String[] args){
-
-        final int num = 5;
-        System.out.println(num);
-
-        // num = 10; // 상수 값이라서 오류 나온다.
-    }
-}
