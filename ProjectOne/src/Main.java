2d2ff0ed6b4a7fcb6e029de59a477e7e7002e067
cjfwls05324 <77821929+cheoljin0324@users.noreply.github.com>
2024-04-25 15:53:02 +0900

    과제 작업

---
+++ ProjectOne/src/Main.java
@@ -0,0 +1,42 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        int age = 20;
+        String Name = "김철진";
+
+        System.out.println(age+Name);*/
+
+        /*
+        String myName = "김철진";
+        int myGrade = 1;
+
+        System.out.println("이름:"+myName + " 학년: "+myGrade);
+        */
+
+        /*
+        float a = 39835.2311f;
+        System.out.println((int)a);
+        */
+
+        /*
+        int a = 10;
+        int b = 20;
+        int temp;
+        temp = a;
+        a= b;
+        b= temp;
+
+        System.out.println(a + "출력 이후"+ b);
+
+        */
+
+        /*
+        final float a = 4.2f;
+        final float b = 3.14f;
+        final int = int(a.2)*(a/2)*b;
+        System.out.println(((a/2)*(a/2)*b));
+        */
+    }
+}
